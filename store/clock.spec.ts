53e955925a42439add289b24c1f152014a2c4ed8
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-01-08 14:46:09 -0500

    add tests

--- store/clock.spec.ts
+++ store/clock.spec.ts
@@ -4,12 +4,15 @@ import {
   clockSlice,
   ClockState,
   getApplicationTime,
+  getApplicationUTC,
+  getMissionTime,
   initialState,
   set,
   start,
 } from "store/clock";
 
 // adds handy matchers for comparing clock times
+// see typings/index.d.ts for the TS interface
 expect.extend({
   toHappenAround(x: Date, y: Date, z: string) {
     const received = x.getTime();
@@ -99,6 +102,77 @@ describe("store/clockSlice", () => {
       expect(received.toDate()).toHappenAround(expectedTime);
 
       clock.uninstall();
+    });
+
+    it("#getApplicationTime should return a diff between when a mission was started and stopped when it isn't running", () => {
+      const applicationTime = new Date(1985, 11, 5, 0, 1, 0);
+      const lastStarted = new Date(2021, 1, 8, 0, 0, 0);
+      // five seconds later
+      const lastStopped = new Date(2021, 1, 8, 0, 0, 5);
+      const s = {
+        isRunning: false,
+        ready: true,
+        applicationTime: applicationTime.toISOString(),
+        lastStarted: lastStarted.toISOString(),
+        lastStopped: lastStopped.toISOString(),
+      } as ClockState;
+
+      // five seconds later
+      const expectedTime = new Date(1985, 11, 5, 0, 1, 5);
+
+      const received = getApplicationTime(s);
+      expect(received.toDate()).toHappenAround(expectedTime);
+    });
+
+    it("#getApplicationTime should return a diff between now and when a mission was started when it is running", () => {
+      const clock = FakeTimers.install({ toFake: ["Date"] });
+
+      const applicationTime = new Date(1985, 11, 5, 0, 1, 0);
+      const lastStopped = new Date(2021, 1, 7, 23, 23, 59);
+      const lastStarted = new Date();
+
+      const s = {
+        isRunning: true,
+        ready: true,
+        applicationTime: applicationTime.toISOString(),
+        lastStopped: lastStopped.toISOString(),
+        lastStarted: lastStarted.toISOString(),
+      } as ClockState;
+
+      // five seconds later
+      const expectedTime = new Date(1985, 11, 5, 0, 1, 5);
+      clock.tick(5000);
+
+      const received = getApplicationTime(s);
+      expect(received.toDate()).toHappenAround(expectedTime);
+
+      clock.uninstall();
+    });
+
+    it("#getApplicationUTC should return null when the clock hasn't been set", () => {
+      const s = {
+        isRunning: true,
+        ready: true,
+        applicationTime: null,
+        lastStopped: null,
+        lastStarted: null,
+      } as ClockState;
+
+      const received = getApplicationUTC(s);
+      expect(received).toBeNull();
+    });
+
+    it("#getMissionTime should return 0 when the clock has not been set", () => {
+      const s = {
+        isRunning: true,
+        ready: true,
+        applicationTime: null,
+        lastStopped: null,
+        lastStarted: null,
+      } as ClockState;
+
+      const received = getMissionTime(s);
+      expect(received).toEqual(0);
     });
   });
 });
