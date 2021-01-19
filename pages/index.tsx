77b4d7ec58e4a5d5f39434d4deb3c7008a164c70
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-01-19 14:37:48 -0500

    clean up boilerplate <head>

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,35 +7,7 @@ function Index({ evas }) {
   return (
     <div>
       <Head>
-        <meta charSet="utf-8" />
         <title>{process.env.TITLE}</title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
-          rel="stylesheet"
-        ></link>
       </Head>
       Welcome to CODA! Try out one of our many EVA replays.
       <ul>
