59e741554bd1990b421b4180082215fcfd7e6558
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-01-19 15:16:05 -0500

    fix missing fonts

--- pages/_app.js
+++ pages/_app.js
@@ -32,7 +32,15 @@ export default function App({ Component, pageProps }) {
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
           rel="stylesheet"
-        ></link>
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Component {...pageProps} />
     </Provider>
