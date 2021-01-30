e345f858fc4af6de71d89fae4d8b0d26a35c2f9b
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-01-30 13:13:37 -0500

    fix the favicon urls

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,11 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
-          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/coda/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/coda/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/coda/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/coda/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/coda/favicon/safari-pinned-tab.svg" color="#5bbad5" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
             rel="stylesheet"
