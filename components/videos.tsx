5c90a7e3451b60c932705000578748b95e2ec194
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-02-01 14:11:04 -0500

    let the waiting event handle pausing instead

--- components/videos.tsx
+++ components/videos.tsx
@@ -164,8 +164,6 @@ export default function Videos() {
       }
 
       if (Math.abs(currentTime - videoStartOffset) > 1) {
-        players[name].current.pause();
-        dispatch(buffering(name));
         players[name].current.currentTime = videoStartOffset;
       }
     });
