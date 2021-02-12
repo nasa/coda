b44cf093eab56e4b3fa93bb3abe23b6c68c4526f
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-02-12 13:48:37 -0500

    mute all videos by default

--- components/video.tsx
+++ components/video.tsx
@@ -23,7 +23,7 @@ export default function Videos({ id }: { id: number }) {
     (state) => state
   );
   const videoElement = useRef() as MutableRefObject<HTMLVideoElement>;
-  const [muted, setMuted] = useState(id !== 1);
+  const [muted, setMuted] = useState(true);
   const [metadata, setMetadata] = useState(null);
   const [status, setStatus] = useState(null);
   const [sourceURL, setSourceURL] = useState("");
