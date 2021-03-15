eeb2b3a5f491bd5e71feef211bb9b77174411be4
Ben Feist <bf@benfeist.com>
2021-03-15 17:59:57 -0500

    Added clockcalc and tweaked clocksync to relax the accuracy because of slow ipads in space.

--- public/clocksync/index.js
+++ public/clocksync/index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
   var t;
 
   waitForTopOfSecond();
@@ -7,14 +7,14 @@ document.addEventListener("DOMContentLoaded", function(){
     //start timer at the top of the next second
     var keepLooking = true;
     var i = 0;
-    while(keepLooking) {
+    while (keepLooking) {
       var currUTCDate = new Date().toISOString();
       var milliseconds = currUTCDate.substring(20, 23);
       if (milliseconds === "000") {
         console.log(currUTCDate);
         keepLooking = false;
         t = setInterval(makeQR, 1000);
-      } 
+      }
     }
   }
 
@@ -22,25 +22,24 @@ document.addEventListener("DOMContentLoaded", function(){
     currTimestamp = new Date();
     var currUTCDate = currTimestamp.toISOString();
     outputStr = currUTCDate;
-    
+
     var typeNumber = 0;
-    var errorCorrectionLevel = 'H';
+    var errorCorrectionLevel = "H";
     var qr = qrcode(typeNumber, errorCorrectionLevel);
-    qr.addData(outputStr, 'Byte');
+    qr.addData(outputStr, "Byte");
     qr.make();
-    document.getElementById('qrcode').innerHTML = qr.createSvgTag({
+    document.getElementById("qrcode").innerHTML = qr.createSvgTag({
       cellSize: 1,
       margin: 1,
-      scalable: true
+      scalable: true,
     });
 
     document.getElementById("headerCenter").innerHTML = outputStr;
 
     var milliseconds = currUTCDate.substring(20, 23);
-    if (parseInt(milliseconds) > 10) {
+    if (parseInt(milliseconds) > 100) {
       clearInterval(t);
       waitForTopOfSecond();
     }
   }
-  
 });
