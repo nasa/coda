9481ef23e337d2fda6eab90f088cba65d9272851
Cameron Pittman <cameron.w.pittman@gmail.com>
2021-03-31 09:44:18 -0400

    add server-side node

---
+++ server/index.ts
@@ -0,0 +1,18 @@
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev, dir: "./client/" });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(3000, () => {
+    console.log("> Ready on http://localhost:3000");
+  });
+});
