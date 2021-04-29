d37ef6769c37ac7ca238ae4f71a4c210b38e220a
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-04-29 17:35:56 -0400

    fix problematic file name

--- pages/api/echo.ts
+++ pages/api/echo.ts
@@ -2,15 +2,16 @@ import _ from "lodash";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 /**
- * /api/test?echo=foo
+ * `/api/echo?word=foo`
+ *
  * Get a query parameter echoed back
  */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const echo = _.get(req.query, "echo", "") as string;
-  const result = await test(echo);
+  const word = _.get(req.query, "word", "") as string;
+  const result = await test(word);
   res.status(200).json({ result });
 }
 
-export async function test(echo: string): Promise<string> {
-  return Promise.resolve("Your query was " + echo);
+export async function test(word: string): Promise<string> {
+  return Promise.resolve(`Your word was '${word}'`);
 }
