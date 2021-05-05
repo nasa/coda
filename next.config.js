f566ef84b4adf2a4f5cfe2413112c9558de4f751
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-05-05 14:20:26 -0400

    update env vars

--- next.config.js
+++ next.config.js
@@ -7,7 +7,9 @@ const envs = {
     IO_HOST: "https://emss.s3-us-gov-east-1.amazonaws.com",
     IO_MOCK_MEDIA_URL: "https://emss.s3-us-gov-east-1.amazonaws.com/coda_data/",
     WIKI_API_URL: "http://wiki-mock/eva/api.php",
-    SPACETRACK_API_URL: "http://coda-data.apolloinrealtime.org/spacetrack_iss/get_iss.php",
+    // seconds to keep the server-side API hits cached (assuming we get a response)
+    CACHE_AGE: "300",
+    CACHE_ROOT: "./server/.cache-local/",
   },
   development: {
     NEXT_PUBLIC_APP_ENV: "development",
@@ -18,9 +20,8 @@ const envs = {
     // FYI, this is the dev wiki, which can be useful for testing changes to the info we're pulling from the wiki
     // WIKI_API_URL: "https://wiki-dev.fit.nasa.gov/iss/api.php",
     WIKI_API_URL: "https://wiki.jsc.nasa.gov/iss/api.php",
-    // seconds to keep the server-side API hits cached (assuming we get a response)
     CACHE_AGE: "300",
-    CACHE_ROOT: "./server/.cache/",
+    CACHE_ROOT: "./server/.cache-dev/",
   },
   production: {
     NEXT_PUBLIC_APP_ENV: "prod",
@@ -31,7 +32,8 @@ const envs = {
     WIKI_API_URL: "https://wiki.jsc.nasa.gov/iss/api.php",
     // FYI, this is the dev wiki, which can be useful for testing changes to the info we're pulling from the wiki
     // WIKI_API_URL: "https://wiki-dev.fit.nasa.gov/iss/api.php",
-    // SPACETRACK_API_URL: "https://coda-dev.fit.nasa.gov/coda_server/spacetrack_iss/get_iss.php",
+    CACHE_AGE: "300",
+    CACHE_ROOT: "./server/.cache/",
   },
 };
 
