d1a434eb873f2d4f809e04b7179de8cbe1dea42e
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-07-14 17:20:39 -0400

    add tests

--- services/cache-client.spec.ts
+++ services/cache-client.spec.ts
@@ -18,7 +18,20 @@ describe("services/cache-client", () => {
     expect(ran).toBeTruthy();
   });
 
-  it("should not execute a retriever async function when something is in the cache", async () => {
+  it("should return data", async () => {
+    const identifier = expect.getState().currentTestName;
+
+    const data = { message: "executed" };
+    const retriever = async () => {
+      return data;
+    };
+
+    const res = await retrieveJSON(identifier, retriever);
+
+    expect(res.data.message).toEqual("executed");
+  });
+
+  it("should not execute a retriever async function when the cache is hot", async () => {
     const identifier = expect.getState().currentTestName;
 
     let runs = 0;
@@ -42,14 +55,20 @@ describe("services/cache-client", () => {
       throw new Error("💥");
     };
 
+    let unhandledErrors = 0;
     try {
       await retrieveJSON(identifier, retriever);
-    } catch (_e) {}
+    } catch (_e) {
+      unhandledErrors += 1;
+    }
     try {
       await retrieveJSON(identifier, retriever);
-    } catch (_e) {}
+    } catch (_e) {
+      unhandledErrors += 1;
+    }
 
     expect(runs).toEqual(2);
+    expect(unhandledErrors).toEqual(2);
   });
 
   it("should run the retriever again if the cache is stale", async () => {
@@ -61,24 +80,22 @@ describe("services/cache-client", () => {
       return {};
     };
 
-    try {
-      await retrieveJSON(identifier, retriever);
-    } catch (_e) {}
+    await retrieveJSON(identifier, retriever);
 
+    // wait 10 ms
     await (async () => {
       return new Promise((resolve) => {
         setTimeout(resolve, 10);
       });
     })();
 
-    try {
-      await retrieveJSON(identifier, retriever, { cacheAge: 0.01 });
-    } catch (_e) {}
+    // only accept cache entries younger than 10 ms. the cache entry must be older than 10 ms given the above wait, so the retriever runs again
+    await retrieveJSON(identifier, retriever, { cacheAge: 0.01 });
 
     expect(runs).toEqual(2);
   });
 
-  it("should not run the retriever again if the cache is hot", async () => {
+  it("should run the retriever again when opts.preferNew", async () => {
     const identifier = expect.getState().currentTestName;
 
     let runs = 0;
@@ -87,24 +104,76 @@ describe("services/cache-client", () => {
       return {};
     };
 
-    try {
-      await retrieveJSON(identifier, retriever);
-    } catch (_e) {}
+    await retrieveJSON(identifier, retriever);
+    await retrieveJSON(identifier, retriever, { preferNew: true });
 
+    expect(runs).toEqual(2);
+  });
+
+  it("should return an error when opts.errorOk", async () => {
+    const identifier = expect.getState().currentTestName;
+
+    const retriever = async () => {
+      throw new Error("Something went wrong");
+    };
+
+    const res = await retrieveJSON(identifier, retriever, { errorOk: true });
+
+    expect(res.error).toEqual("Error: Something went wrong");
+  });
+
+  it("should not cache even when opts.errorOk", async () => {
+    const identifier = expect.getState().currentTestName;
+
+    let ran = 0;
+    const retriever = async () => {
+      ran += 1;
+      throw new Error("Something went wrong");
+    };
+
+    await retrieveJSON(identifier, retriever, { errorOk: true });
+    await retrieveJSON(identifier, retriever, { errorOk: true });
+
+    expect(ran).toEqual(2);
+  });
+
+  it("should return cached data when the cache is stale, an error occurs, and opts.staleOk", async () => {
+    const identifier = expect.getState().currentTestName;
+
+    // turn off warning messages about stale data for this test
+    const old = console.warn;
+    console.warn = () => {};
+
+    let ran = 0;
+    const retriever = async () => {
+      ran += 1;
+      if (ran === 1) {
+        // works the first time
+        return { message: "Worked!" };
+      }
+      // fails the second time
+      throw new Error("Something went wrong");
+    };
+
+    await retrieveJSON(identifier, retriever);
+
+    // wait 10 ms
     await (async () => {
       return new Promise((resolve) => {
         setTimeout(resolve, 10);
       });
     })();
 
-    try {
-      await retrieveJSON(identifier, retriever, { cacheAge: 0.01 });
-    } catch (_e) {}
+    const res = await retrieveJSON(identifier, retriever, { cacheAge: 0.01, staleOk: true });
 
-    expect(runs).toEqual(2);
+    expect(ran).toEqual(2);
+    expect(res.data.message).toEqual("Worked!");
+
+    // reset console.warn
+    console.warn = old;
   });
 
-  // ensure we're starting with a clean slate and cleaning up after ourselves
+  // ensure we're starting with a clean cache and cleaning up after ourselves
   beforeAll(() => {
     cacache.rm.all(process.env.CACHE_ROOT);
   });
