cc38053871ebf48cbcf379cee456ce4d34a4689a
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-07-20 09:10:18 -0500

    improve homepage layout

--- components/button.tsx
+++ components/button.tsx
@@ -3,14 +3,21 @@ import styles from "./button.module.css";
 
 export interface Options {
   style?: string;
+  callback: () => void;
 }
 
 const defaults: Options = {
   style: "grey",
+  callback: () => {},
 };
 
+/** Render a styled button to perform an action on a page. It SHOULD NOT be used to change pages. That's what <a /> is for */
 export default function Button(options: React.PropsWithChildren<Options>) {
   const opts = { ...defaults, ...options };
 
-  return <button className={`${styles.button} ${styles[opts.style]}`}>{opts.children}</button>;
+  return (
+    <button onClick={opts.callback} className={`${styles.button} ${styles[opts.style]}`}>
+      {opts.children}
+    </button>
+  );
 }
