771f428e3a47641e28f74f9102bf616c21f0a940
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-07-20 09:10:18 -0500

    improve homepage layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,55 @@
 import Head from "next/head";
-// import Link from "next/link";
+import Link from "next/link";
 import Button from "components/button";
 import styles from "./index.module.css";
 
 export default function Index() {
   return (
-    <div className={styles.container}>
+    <div className={styles.main}>
       <Head>
         <title>{process.env.TITLE}</title>
       </Head>
-      <div className={styles.intro}>
-        <div className={styles.description}>
-          <div className={styles.logo}>
-            <div className={styles.verticalCenter}>
-              <img className={styles.meatball} src="/images/logo_NASA.svg" alt="NASA meatball" />
+      <div className={styles.container}>
+        <div className={styles.verticalCenter}>
+          <div className={styles.description}>
+            <div className={styles.logo}>
+              <div className={styles.verticalCenter}>
+                <img className={styles.meatball} src="/images/logo_NASA.svg" alt="NASA meatball" />
+              </div>
+              <div className={styles.verticalCenter}>
+                <span className={styles.wordMark}>CODA</span>
+              </div>
             </div>
-            <div className={styles.verticalCenter}>
-              <span className={styles.wordMark}>CODA</span>
+            <div className={styles.tourText}>
+              <p>
+                Lorem ipsump dolor sit amet. Yo this is a story all about how my life got
+                twist-turned upside down. Now let me take a second; y'all just sit right there.
+              </p>
             </div>
-          </div>
-          <div className={styles.tourText}>
-            <p>
-              Lorem ipsump dolor sit amet. Yo this is a story all about how my life got twist-turned
-              upside down. Now let me take a second; y'all sit right there.
-            </p>
-          </div>
-          <div style={{ marginTop: "10px" }}>
-            <Button>TOUR CODA</Button>
+            <div style={{ marginTop: "10px" }}>
+              <Button>TOUR CODA</Button>
+            </div>
           </div>
         </div>
-        <div className={styles.sources}>
-          <div className={styles.sourcesPanel}>
-            <span className={styles.greyedOut}>Select a Source</span>
-            <ul className={styles.ul}>
-              <li className={styles.li}>
-                <a href="/view">ISS</a>
-              </li>
-              <li className={styles.li}>
-                <a href="/view/test-events">Test Events</a>
-              </li>
-              <li className={styles.li}>
-                <a href="/view/nbl">NBL</a>
-              </li>
-            </ul>
+        <div className={styles.verticalCenter}>
+          <div className={styles.sources}>
+            <div className={styles.sourcesPanel}>
+              <span className={styles.sourcesHeader}>Select a Source</span>
+              <ul className={styles.ul}>
+                <li className={styles.li}>
+                  <Link href="/view">ISS</Link>
+                </li>
+                <li className={styles.li}>
+                  <Link href="/view/test-events">Test Events</Link>
+                </li>
+                <li className={styles.li}>
+                  <Link href="/view/nbl">NBL</Link>
+                </li>
+                <li className={styles.li}>
+                  <Link href="/view/nbl">Artemis</Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
