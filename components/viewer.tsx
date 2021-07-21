afaa7bce0d10295a030970cdcc28aa3c226dbdf8
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-07-21 18:09:45 -0500

    add more layouts

--- components/viewer.tsx
+++ components/viewer.tsx
@@ -3,8 +3,8 @@ import { useSelector } from "react-redux";
 import { allLayouts } from "store/viewer";
 import styles from "./viewer.module.css";
 
-export function Frame({ width, height }: { width: number; height: number }) {
-  return <div style={{ width, height }}></div>;
+export function Frame() {
+  return <div className={styles.frame}></div>;
 }
 
 export default function Viewer() {
@@ -12,9 +12,14 @@ export default function Viewer() {
   const layoutDefinition = allLayouts[selectedLayout];
 
   const frames = [];
-  for (let i = 0; i <= layoutDefinition.frames; i++) {
-    frames.push(<div className={`${styles.frame} ${styles[`f${i}`]}`}>{i}</div>);
+  for (let i = 1; i <= layoutDefinition.frames; i++) {
+    const gridAreaName = styles[`f${i}`];
+    frames.push(
+      <div className={`${styles.frameContainer} ${gridAreaName}`}>
+        <Frame />
+      </div>
+    );
   }
 
-  return <div className={`${styles.main} ${styles.layout1}`}>{frames}</div>;
+  return <div className={`${styles.main} ${styles[`layout${selectedLayout}`]}`}>{frames}</div>;
 }
