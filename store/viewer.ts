d5d45bba5a77b256b625184f694f948edcb69bd4
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-07-21 17:54:01 -0500

    simplify handling layouts

--- store/viewer.ts
+++ store/viewer.ts
@@ -2,71 +2,26 @@
 
 import { createSlice } from "@reduxjs/toolkit";
 
-/** (row, column) form defining a set of frames in the viewer */
-export interface Frames {
-  /** Row */
-  [key: number]: {
-    /** Column */
-    [key: number]: {
-      /** Numerator width / 12. eg. `12` is 100% width, `6` is 50% width */
-      width: number;
-      /** Numerator height / 12. eg. `12` is 100% height, `6` is 50% height */
-      height: number;
-    };
-  };
-}
-
 /** Definition of all possible layouts */
 export interface Layouts {
   [key: number]: {
     svg: string;
-    frames: Frames;
+    frames: number;
   };
 }
 
 export const allLayouts: Layouts = {
   0: {
     svg: "/icons/layout1.svg",
-    frames: [
-      [
-        {
-          width: 12,
-          height: 12,
-        },
-      ],
-    ],
+    frames: 6,
   },
   1: {
     svg: "/icons/layout1.svg",
-    frames: [
-      [
-        {
-          width: 6,
-          height: 12,
-        },
-      ],
-      [
-        {
-          width: 6,
-          height: 12,
-        },
-      ],
-    ],
+    frames: 2,
   },
   2: {
     svg: "/icons/layout1.svg",
-    frames: [
-      [
-        {
-          width: 8,
-          height: 12,
-        },
-      ],
-      [
-        { width: 4, height: 6 },
-        { width: 4, height: 6 },
-      ],
-    ],
+    frames: 6,
   },
 };
 
