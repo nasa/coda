2587ac4a988f02a6abd1847d05df52a7a67a640a
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-07-21 15:35:39 -0500

    improve styling the layout picker

--- components/layout-picker.tsx
+++ components/layout-picker.tsx
@@ -1,13 +1,50 @@
 import styles from "./layout-picker.module.css";
 
-export default function LayoutPicker({ closeClick }: { closeClick: () => void }) {
+export default function LayoutPicker({ closeClick }: { closeClick?: () => void }) {
   return (
     <div className={styles.main}>
       <div className={styles.top}>
-        <div>Layout Picker</div>
-        <div onClick={closeClick}>x</div>
+        <div>Select a Layout</div>
+        {closeClick && <div onClick={closeClick}>✕</div>}
       </div>
-      LAYOUTS
+      <div className={styles.layouts}>
+        <div className={styles.layout}>
+          <img src="/icons/layout1.svg" alt="Layout 1" />
+        </div>
+        <div className={styles.layout}>
+          <img src="/icons/layout1.svg" alt="Layout 1" />
+        </div>
+        <div className={styles.layout}>
+          <img src="/icons/layout1.svg" alt="Layout 1" />
+        </div>
+        <div className={styles.layout}>
+          <img src="/icons/layout1.svg" alt="Layout 1" />
+        </div>
+        <div className={styles.layout}>
+          <img src="/icons/layout1.svg" alt="Layout 1" />
+        </div>
+        <div className={styles.layout}>
+          <img src="/icons/layout1.svg" alt="Layout 1" />
+        </div>
+        <div className={styles.layout}>
+          <img src="/icons/layout1.svg" alt="Layout 1" />
+        </div>
+        <div className={styles.layout}>
+          <img src="/icons/layout1.svg" alt="Layout 1" />
+        </div>
+        <div className={styles.layout}>
+          <img src="/icons/layout1.svg" alt="Layout 1" />
+        </div>
+        <div className={styles.layout}>
+          <img src="/icons/layout1.svg" alt="Layout 1" />
+        </div>
+        <div className={styles.layout}>
+          <img src="/icons/layout1.svg" alt="Layout 1" />
+        </div>
+        <div className={styles.layout}>
+          <img src="/icons/layout1.svg" alt="Layout 1" />
+        </div>
+      </div>
     </div>
   );
 }
