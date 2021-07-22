e6f84e089108aaa3c3638f6ff97be9fdcf51f6d1
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-07-22 13:18:01 -0500

    add swapping out frame content

--- components/frame.tsx
+++ components/frame.tsx
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import { useSelector } from "react-redux";
 import { PseudoDropdown } from "components/dropdown-v2";
+import EVAInfo from "components/eva-info";
 import FramePickerModal, { FrameLabel } from "components/frame-picker";
 import styles from "./frame.module.css";
 
@@ -30,6 +31,16 @@ export function FrameHeader({ frameID, frameTypeID }: { frameID: number; frameTy
   );
 }
 
+const frameTypesToElements = {
+  0: () => <>0: ISS Video Downlink</>,
+  1: () => <>1: ISS Video Non-Downlink</>,
+  2: () => <>2: ISS Photography</>,
+  3: () => <>3: ISS Groundtrack</>,
+  4: EVAInfo,
+  5: () => <>5: Doug</>,
+  6: () => <>6: ISS Telemetry</>,
+};
+
 /** Identify the frame */
 export interface Options {
   id: number;
@@ -39,10 +50,15 @@ export interface Options {
 export default function Frame(options: React.PropsWithChildren<Options>) {
   const frameTypeID = useSelector((state) => state.viewer.frames[options.id]);
 
+  let FrameRender = () => <>{options.id}</>;
+  if (!_.isNil(frameTypeID)) {
+    FrameRender = frameTypesToElements[frameTypeID];
+  }
+
   return (
     <div className={styles.main}>
       <FrameHeader frameID={options.id} frameTypeID={frameTypeID} />
-      {options.id}
+      <FrameRender />
     </div>
   );
 }
