0262485092d856f3152779c7395532bdf7999f85
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-07-22 13:25:30 -0500

    add custom controls to frames

--- components/eva-info.tsx
+++ components/eva-info.tsx
@@ -1,3 +1,9 @@
+import styles from "./eva-info.module.css";
+
+export function EVAInfoControls() {
+  return <>EVA Info Controls</>;
+}
+
 export default function EVAInfo() {
-  return <div>EVA Info</div>;
+  return <div className={styles.main}>EVA Info</div>;
 }
--- components/frame.tsx
+++ components/frame.tsx
@@ -1,16 +1,21 @@
 import _ from "lodash";
 import { useSelector } from "react-redux";
 import { PseudoDropdown } from "components/dropdown-v2";
-import EVAInfo from "components/eva-info";
+import EVAInfo, { EVAInfoControls } from "components/eva-info";
 import FramePickerModal, { FrameLabel } from "components/frame-picker";
 import styles from "./frame.module.css";
 
+interface Options {
+  frameID: number;
+  frameTypeID: number;
+}
+
 /** Renders the header for a frame */
-export function FrameHeader({ frameID, frameTypeID }: { frameID: number; frameTypeID: number }) {
+export function FrameHeader(options: React.PropsWithChildren<Options>) {
   let label = <>&nbsp;Pick a source</>;
 
-  if (!_.isNil(frameTypeID)) {
-    label = <FrameLabel frameTypeID={frameTypeID} />;
+  if (!_.isNil(options.frameTypeID)) {
+    label = <FrameLabel frameTypeID={options.frameTypeID} />;
   }
 
   return (
@@ -21,22 +26,33 @@ export function FrameHeader({ frameID, frameTypeID }: { frameID: number; frameTy
             color="grey"
             size="skinny"
             modal={FramePickerModal}
-            modalOptions={{ frameID }}
+            modalOptions={{ frameID: options.frameID }}
           >
             {label}
           </PseudoDropdown>
         </div>
       </div>
+      {options.children}
     </div>
   );
 }
 
-const frameTypesToElements = {
+const frameTypeIDsToRenders = {
   0: () => <>0: ISS Video Downlink</>,
   1: () => <>1: ISS Video Non-Downlink</>,
   2: () => <>2: ISS Photography</>,
   3: () => <>3: ISS Groundtrack</>,
   4: EVAInfo,
+  5: () => <>5: Doug</>,
+  6: () => <>6: ISS Telemetry</>,
+};
+
+const frameTypeIDsToControls = {
+  0: () => <>0: ISS Video Downlink</>,
+  1: () => <>1: ISS Video Non-Downlink</>,
+  2: () => <>2: ISS Photography</>,
+  3: () => <>3: ISS Groundtrack</>,
+  4: EVAInfoControls,
   5: () => <>5: Doug</>,
   6: () => <>6: ISS Telemetry</>,
 };
@@ -51,13 +67,17 @@ export default function Frame(options: React.PropsWithChildren<Options>) {
   const frameTypeID = useSelector((state) => state.viewer.frames[options.id]);
 
   let FrameRender = () => <>{options.id}</>;
+  let FrameControls = () => <>Controls for {options.id}</>;
   if (!_.isNil(frameTypeID)) {
-    FrameRender = frameTypesToElements[frameTypeID];
+    FrameRender = frameTypeIDsToRenders[frameTypeID];
+    FrameControls = frameTypeIDsToControls[frameTypeID];
   }
 
   return (
     <div className={styles.main}>
-      <FrameHeader frameID={options.id} frameTypeID={frameTypeID} />
+      <FrameHeader frameID={options.id} frameTypeID={frameTypeID}>
+        <FrameControls />
+      </FrameHeader>
       <FrameRender />
     </div>
   );
