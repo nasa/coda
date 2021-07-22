89db62d7dd05afe6e8c570bf94bb5abc8b75e7c9
Cameron Pittman <cameron.w.pittman@nasa.gov>
2021-07-22 13:08:57 -0500

    improve handling dropdown styles; add frame type state to store

--- components/frame-picker.tsx
+++ components/frame-picker.tsx
@@ -1,3 +1,6 @@
+import _ from "lodash";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faCamera,
@@ -10,9 +13,7 @@ import {
   faVideo,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import _ from "lodash";
-import { useSelector } from "react-redux";
-import { allFrames } from "store/viewer";
+import { allFrames, selectFrameType } from "store/viewer";
 import styles from "./frame-picker.module.css";
 
 library.add(
@@ -57,9 +58,17 @@ const frameDecoration = {
   },
 };
 
-export function FrameSelection({ id }: { id: number }) {
-  const { title } = allFrames[id];
-  const { icon, color } = frameDecoration[id];
+/**
+ * Renders the label for a type of frame
+ */
+export function FrameLabel({
+  frameTypeID,
+}: {
+  /** ID of the type of frame */
+  frameTypeID: number;
+}) {
+  const { title } = allFrames[frameTypeID];
+  const { icon, color } = frameDecoration[frameTypeID];
 
   return (
     <div className={styles.item}>
@@ -71,17 +80,39 @@ export function FrameSelection({ id }: { id: number }) {
   );
 }
 
-export default function FramePicker() {
+/** Renders a modal with a list of frame types to choose from */
+export default function FramePickerModal({
+  closeClick,
+  options: { frameID },
+}: {
+  closeClick: () => void;
+  options: { frameID: number };
+}) {
+  const dispatch = useDispatch();
   const selectedSource = useSelector((state) => state.viewer.selectedSource);
 
+  const handleSelectFrameType = (frameTypeID: number) => (e: React.MouseEvent) => {
+    e.preventDefault();
+    dispatch(selectFrameType({ frameID, frameTypeID }));
+    closeClick();
+  };
+
   const availableFrames = Object.keys(allFrames).filter(
-    (id) => allFrames[id].source === selectedSource
+    (frameTypeID) => allFrames[frameTypeID].source === selectedSource
   );
 
   return (
     <div className={styles.main}>
       {availableFrames.length > 0 ? (
-        availableFrames.map((id) => <FrameSelection id={+id} />)
+        availableFrames.map((frameTypeID) => (
+          <div
+            className={styles.option}
+            onClick={handleSelectFrameType(+frameTypeID)}
+            key={`FRAME__PICKER__${frameID}__${frameTypeID}`}
+          >
+            <FrameLabel frameTypeID={+frameTypeID} />
+          </div>
+        ))
       ) : (
         <span>No available sources</span>
       )}
--- components/frame.tsx
+++ components/frame.tsx
@@ -1,18 +1,28 @@
+import _ from "lodash";
+import { useSelector } from "react-redux";
 import { PseudoDropdown } from "components/dropdown-v2";
-import FramePicker from "components/frame-picker";
+import FramePickerModal, { FrameLabel } from "components/frame-picker";
 import styles from "./frame.module.css";
 
-export interface Options {
-  id: number;
-}
+/** Renders the header for a frame */
+export function FrameHeader({ frameID, frameTypeID }: { frameID: number; frameTypeID: number }) {
+  let label = <>&nbsp;Pick a source</>;
 
-export function FrameHeader() {
+  if (!_.isNil(frameTypeID)) {
+    label = <FrameLabel frameTypeID={frameTypeID} />;
+  }
+
   return (
     <div className={styles.header}>
       <div>
         <div className={styles.dropdown}>
-          <PseudoDropdown color="grey" size="skinny" modal={FramePicker}>
-            Pick a source
+          <PseudoDropdown
+            color="grey"
+            size="skinny"
+            modal={FramePickerModal}
+            modalOptions={{ frameID }}
+          >
+            {label}
           </PseudoDropdown>
         </div>
       </div>
@@ -20,10 +30,18 @@ export function FrameHeader() {
   );
 }
 
+/** Identify the frame */
+export interface Options {
+  id: number;
+}
+
+/** Renders a frame in the viewer */
 export default function Frame(options: React.PropsWithChildren<Options>) {
+  const frameTypeID = useSelector((state) => state.viewer.frames[options.id]);
+
   return (
     <div className={styles.main}>
-      <FrameHeader />
+      <FrameHeader frameID={options.id} frameTypeID={frameTypeID} />
       {options.id}
     </div>
   );
