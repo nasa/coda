0ef38070264f2ece4b8dc9e60beb342947f695b6
Ben Feist <benjamin.f.feist@nasa.gov>
2021-10-25 20:30:55 -0500

    eliminated rejection of bad certs in fetches

--- utils/fetch-with-timeout.ts
+++ utils/fetch-with-timeout.ts
@@ -1,27 +1,40 @@
 /** See https://dmitripavlutin.com/timeout-fetch-request/ */
 
 import AbortController from "abort-controller";
+import fetch from "node-fetch";
+import https from "https";
 
 /**
  * Perform a fetch request that throws if it takes too much time. Timeout defaults to 8 seconds. Usage:
  * `fetchWithTimeout("/url", { timeout: 8000, ...usualFetchOptions })`
  */
 export default async function fetchWithTimeout(
-  input: RequestInfo,
-  options: RequestInit & {
-    /** Milliseconds to timeout */
-    timeout?: number;
-  }
+  input: string,
+  options:
+    | RequestInit
+    | (any & {
+        /** Milliseconds to timeout */
+        timeout?: number;
+      })
 ) {
   const { timeout = 8000 } = options;
 
   const controller = new AbortController();
   const id = setTimeout(() => controller.abort(), timeout);
 
-  const response = await fetch(input, {
-    ...options,
-    signal: controller.signal,
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
   });
+  const response = await fetch(input, {
+    headers: options.headers,
+    method: "GET",
+    agent: httpsAgent,
+  });
+
+  // const response = await fetch(input, {
+  //   ...options,
+  //   signal: controller.signal,
+  // });
   clearTimeout(id);
 
   return response;
