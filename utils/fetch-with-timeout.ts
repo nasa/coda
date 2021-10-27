611e6073a171773ced95c36f0bbf68f076d0a9b7
Ben Feist <benjamin.f.feist@nasa.gov>
2021-10-27 10:42:06 -0500

    Enforce certs when in production

--- utils/fetch-with-timeout.ts
+++ utils/fetch-with-timeout.ts
@@ -22,8 +22,11 @@ export default async function fetchWithTimeout(
   const controller = new AbortController();
   const id = setTimeout(() => controller.abort(), timeout);
 
+  // To avoid invalid cert errors in development environments, don't reject unauthorized certs when in development
+  const rejectUnauthorized = process.env.NODE_ENV === "production" ? true : false;
+
   const httpsAgent = new https.Agent({
-    rejectUnauthorized: false,
+    rejectUnauthorized: rejectUnauthorized,
   });
   const response = await fetch(input, {
     headers: options.headers,
