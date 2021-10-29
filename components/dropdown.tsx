d231ed11cad55dce540d57635099713447bb0514
Ben Feist <benjamin.f.feist@nasa.gov>
2021-10-29 15:08:31 -0500

    limited EVA dropdown to cut off anything before 2013-03-30

--- components/dropdown.tsx
+++ components/dropdown.tsx
@@ -37,6 +37,7 @@ export default function EVADropdown() {
   // TODO: add ... to avoid going behind the arrow
 
   const today = new Date();
+  const earliestCutoff = new Date("2013-03-30");
 
   return (
     <div className={styles.select}>
@@ -53,10 +54,10 @@ export default function EVADropdown() {
         ) : (
           allEVAs
             .filter((eva) => {
-              // don't show future EVAs
+              // don't show future EVAs or EVS before 2013-03-30 (because of IO data being unavailable before that)
               const [year, month, day] = eva.startDate.split("-").map(Number);
               const dateOfEVA = new Date(Date.UTC(year, month - 1, day));
-              return diff(today, dateOfEVA) > 0;
+              return diff(today, dateOfEVA) > 0 && diff(earliestCutoff, dateOfEVA) < 0;
             })
             // sort most recent to oldest
             .reverse()
