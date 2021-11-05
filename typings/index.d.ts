bc9feb6efdf901e008ce0ce0fb24eb7472080a7c
Ben Feist <benjamin.f.feist@nasa.gov>
2021-11-05 15:38:36 -0500

    Added NBL collection to typings. Created parser to find EV1,2,QUAD

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -105,7 +105,7 @@ export enum Collection {
   /** JSC Rock Yard. https://io.jsc.nasa.gov/app/collections.cfm?cid=2359937 (why the quoted string with spaces? so it matches the Test Environment name in the exploration wiki */
   "JSC Rock Yard" = 2359937,
   /** Neutral Buoyancy Lab. https://io.jsc.nasa.gov/app/collections.cfm?cid=2359935 */
-  NBL = 2359935,
+  NBL = 78178,
   /** Artificial Reduced Gravity Offload System. https://io.jsc.nasa.gov/app/collections.cfm?cid=2359933 */
   ARGOS = 2359933,
   /** NASA Extreme Environment Mission Operations. https://io.jsc.nasa.gov/app/collections.cfm?cid=2359936 */
