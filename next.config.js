cd4f82df52298cf8eb24311a7d6d2a35989bc11a
Ben Feist <benjamin.f.feist@nasa.gov>
2021-11-18 18:03:10 -0600

    Added fix to stop next from nuking each page to save memory

--- next.config.js
+++ next.config.js
@@ -8,4 +8,10 @@ module.exports = {
       },
     ];
   },
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 2500 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 20,
+  },
 };
