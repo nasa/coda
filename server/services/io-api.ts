6cda27848354421ed05013e0ba0fe902bd5b2ca5
Ben Feist <benjamin.f.feist@nasa.gov>
2021-11-23 14:15:10 -0600

    Cover NBL downlink numbers for NBL runsn added to multiple collections

--- server/services/io-api.ts
+++ server/services/io-api.ts
@@ -163,14 +163,20 @@ function parseVideoResultMetadata(doc: Doc, collection: Collection): VideoFile {
       downlink = 2;
     }
   } else if (+Collection[collection] === +Collection.NBL) {
-    //modify downlink numbers for nbl collection results based on strings in collections list
-    const fullCollectionsString = doc.collections_string[doc.collections_string.length - 1];
-    if (fullCollectionsString.includes("EV1")) {
-      downlink = 0;
-    } else if (fullCollectionsString.includes("EV2")) {
-      downlink = 1;
-    } else if (fullCollectionsString.includes("QUAD")) {
-      downlink = 2;
+    // Modify downlink numbers for nbl collection results based on strings in collections list
+    // Look through every collection string in the collection_string array. This covers when NBL runs have been added to multiple collections
+    for (let i = 0; i < doc.collections_string.length; i++) {
+      const thisCollectionsString = doc.collections_string[i];
+      if (thisCollectionsString.includes("EV1")) {
+        downlink = 0;
+        break;
+      } else if (thisCollectionsString.includes("EV2")) {
+        downlink = 1;
+        break;
+      } else if (thisCollectionsString.includes("QUAD")) {
+        downlink = 2;
+        break;
+      }
     }
   }
 
