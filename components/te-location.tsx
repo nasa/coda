f88a7ebe74322d9ae8b400604a161df87c91a199
Ben Feist <benjamin.f.feist@nasa.gov>
2021-11-24 16:10:01 -0600

    Filter null slope values

--- components/te-location.tsx
+++ components/te-location.tsx
@@ -153,11 +153,15 @@ export default function TELocation() {
         isoStringFromAnyDateString(markerGPSPoint.time.toString()).split(".")[0] + "Z"
       ).split("T");
 
+      const slopeVal =
+        gpsTracks[track].slopes[markerIndex] === null
+          ? "0.000"
+          : gpsTracks[track].slopes[markerIndex].toFixed(3).toString();
       const items: mapInfoDisplayItems = {
         lat: markerGPSPoint.lat.toFixed(6),
         lng: markerGPSPoint.lon.toFixed(6),
         ele: markerGPSPoint.ele.toFixed(2).toString(),
-        slope: gpsTracks[track].slopes[markerIndex].toFixed(3).toString(),
+        slope: slopeVal,
         date: timestampArr[0],
         time: timestampArr[1],
         hdg: "",
