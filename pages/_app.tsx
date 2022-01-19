227c6d5f2295d1d55d05f10a5c413cea42bd933c
Ben Feist <benjamin.f.feist@nasa.gov>
2022-01-19 17:43:04 -0600

    lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,11 @@ import "../styles.css";
 // The following import prevents a Font Awesome icon server-side rendering bug,
 // where the icons flash from a very large icon down to a properly sized one:
 import "@fortawesome/fontawesome-svg-core/styles.css";
-// Prevent fontawesome from adding its CSS since we did it manually above:
 import { config } from "@fortawesome/fontawesome-svg-core";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }: AppProps) {
-  config.autoAddCss = false; /* eslint-disable import/first */
+  config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since we did it manually above.
 
   const store = useStore(clone(initialState));
 
