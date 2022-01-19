92ef5a49b58f13548c05a1080fa4ec1c37dfd28c
Ben Feist <benjamin.f.feist@nasa.gov>
2022-01-19 17:38:25 -0600

    Added fontawesome icon size fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,16 @@ import { Provider } from "react-redux";
 import { useStore, initialState } from "store";
 import "../styles.css";
 
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }: AppProps) {
+  config.autoAddCss = false; /* eslint-disable import/first */
+
   const store = useStore(clone(initialState));
 
   return (
