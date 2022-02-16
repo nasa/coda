249e48b4e1a3486e6ef4eb690ab71116ce5ad871
James Montalvo <edwin.j.montalvo@nasa.gov>
2022-02-15 22:33:56 -0600

    test: fix running tests on windows, jest needs to load .node files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: "ts-jest/presets/js-with-ts",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "node"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",
