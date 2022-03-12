538a7bd9baf53a7cd6418f95746181b451d94873
Ben Feist <benjamin.f.feist@nasa.gov>
2022-03-11 21:25:56 -0600

    Updated type packages. Changed event dropdown to hack a real window.location change.

--- components/interface/dropdown-event.tsx
+++ components/interface/dropdown-event.tsx
@@ -7,7 +7,7 @@ import { changeDate, diff, isSameDate } from "store/playhead";
 import styles from "./dropdown-event.module.css";
 import { sequencesSelector } from "store/sequences";
 import { padZeros } from "utils/formatting";
-import { Collection } from "utils/enums";
+import { Collection, SourceShortVal } from "utils/enums";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function EventDropdown(props: {
@@ -16,6 +16,7 @@ export default function EventDropdown(props: {
 }) {
   const sequences: SequencesEntityState = useSelector((state: RootState) => state.sequences);
   const date = useSelector((state: RootState) => state.playhead.date);
+  const framework = useSelector((state: RootState) => state.framework);
 
   let allSequences = sequencesSelector.selectAll(sequences);
   if (props.collection === Collection.NBL) {
@@ -37,7 +38,7 @@ export default function EventDropdown(props: {
   const dispatch = useDispatch();
 
   /**
-   * Navigate to another EVA
+   * Navigate to another Event
    */
   const handleEVASelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
     e.preventDefault();
@@ -45,8 +46,13 @@ export default function EventDropdown(props: {
     if (e.target.value !== "") {
       const [year, month, day] = e.target.value.split("-");
       const formattedDate = `${year}-${padZeros(+month, 2)}-${padZeros(+day, 2)}`;
-      dispatch(changeDate(formattedDate));
-      props.setHelpLoaderOpen(true);
+
+      const sourceParam = SourceShortVal[framework.source];
+      /**TODO: This is a hack to get around the fact that weird things happen when you jump between EVA days by dispatching changeDate.
+      Lots of null tle values and stuff. */
+      window.location.assign(`${window.location.pathname}?date=${formattedDate}&s=${sourceParam}`);
+      // dispatch(changeDate(formattedDate));
+      // props.setHelpLoaderOpen(true);
     }
   };
 
