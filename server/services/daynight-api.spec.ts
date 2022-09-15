2f807980162960cdbe480f0375a8d374a6016048
Jackie Vu <jackie.vu@nasa.gov>
2022-09-15 16:30:36 -0500

    wip topo structure

--- server/services/daynight-api.spec.ts
+++ server/services/daynight-api.spec.ts
@@ -1,29 +1,29 @@
-import { topoURL } from "server/services/daynight-api";
+import { getTopoURL } from "server/services/daynight-api";
 
-describe("function topoURL()", () => {
+describe("function getTopoURL()", () => {
   const now = new Date();
   it("should return null", () => {
     const requestDate = new Date(
       Date.UTC(now.getUTCFullYear(), now.getUTCMonth() + 3, now.getUTCDate())
     );
-    expect(topoURL(requestDate)).toBeNull();
+    expect(getTopoURL(requestDate)).toBeNull();
   });
   it("should return blank", () => {
     const requestDate = new Date(Date.UTC(2000, 1, 1));
-    expect(topoURL(requestDate)).toEqual("");
+    expect(getTopoURL(requestDate)).toEqual("");
   });
   it("should return predicted url", () => {
     const requestDate = new Date(
       Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 7)
     );
-    let url = topoURL(requestDate);
+    let url = getTopoURL(requestDate);
     expect(url.includes("/data/stp/")).toBeTruthy();
   });
   it("should return historic (best estimated trajectory) url", () => {
     const requestDate = new Date(
       Date.UTC(now.getUTCFullYear(), now.getUTCMonth() - 2, now.getUTCDate())
     );
-    let url = topoURL(requestDate);
+    let url = getTopoURL(requestDate);
     expect(url.includes("/data/bet/")).toBeTruthy();
   });
 });
