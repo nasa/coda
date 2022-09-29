6fc5ed0db13a0941b65067cb7a5287b20e549a80
Ben Feist <bf@benfeist.com>
2022-09-29 12:12:07 -0400

    lint

--- components/panes/graph/graph.tsx
+++ components/panes/graph/graph.tsx
@@ -1,6 +1,6 @@
 import { HelpButton } from "components/interface/pane-help-control-button";
 import HelpOverlay from "components/interface/pane-help-overlay";
-import { useEffect, useState, memo } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPaneStateValue } from "store/framework";
 import { RootState } from "store/index";
