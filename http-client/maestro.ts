0bfe7e147417a45536aaf963f107623bd6298a17
Benjamin Feist <benjamin.f.feist@nasa.gov>
2023-06-07 11:09:47 -0500

    maestro URL fix

--- http-client/maestro.ts
+++ http-client/maestro.ts
@@ -5,7 +5,7 @@
 export async function getMaestroExecuteTimelineStatus(
   executeEventUuid: string
 ): Promise<WrappedResponse<MaestroInternalAPIData>> {
-  const res = await fetch(`/api/maestro/executeTimelineStatus?uuid${executeEventUuid}`);
+  const res = await fetch(`/api/maestro/executeTimelineStatus?uuid=${executeEventUuid}`);
   const executeTimelineStatus = await res.json();
   return executeTimelineStatus;
 }
