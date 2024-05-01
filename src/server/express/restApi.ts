08dc943b1a5ff6442e8f5bf11b319e0b9be6af27
James Montalvo <edwin.j.montalvo@nasa.gov>
2024-05-01 10:57:15 -0500

    test: add /api/v1/media/manifest-manifest test endpoint

--- src/server/express/restApi.ts
+++ src/server/express/restApi.ts
@@ -29,6 +29,40 @@ app.use("/api/v1/location/iss", locationIssRoute);
 app.use("/api/v1/maestro/executeTimelineStatus", executeTimelineStatusRoute);
 app.use("/api/v1/media/photos", photosRoute);
 app.use("/api/v1/media/videos", videosRoute);
+app.use("/api/v1/media/manifest-manifest", (req, res) => {
+  const start = new Date();
+  start.setUTCHours(0, 0, 0, 0);
+  const startMs = start.getTime();
+
+  const msToday = Date.now() - startMs;
+  const fourMinutes = 4 * 60 * 1000;
+  const fiveMinutes = 5 * 60 * 1000;
+
+  let currentToday = 0;
+
+  const videos: VideoFile[] = [];
+  while (currentToday < msToday) {
+    const currentUtcMs = startMs + currentToday;
+    videos.push({
+      id: "vid" + currentToday,
+      description: `The fake video at ${currentToday}ms`,
+      collection: "Rick Astley",
+      collections: "Rick Astley",
+      dataURL: "https://www.youtube.com/watch?v=oHg5SJYRHA0",
+      mediaLowResURL: "https://en.wikipedia.org/wiki/Rickrolling#/media/File:RickRoll.png",
+      start: currentUtcMs,
+      end: currentUtcMs + fourMinutes,
+      downlink: 7,
+      LOS: false,
+      priority: 1,
+      startDateTime: new Date(currentUtcMs).toISOString(),
+    });
+    currentToday += fiveMinutes;
+  }
+
+  res.status(200).json(videos);
+  return;
+});
 app.use("/api/v1/sequences/evas", evasRoute);
 app.use("/api/v1/sequences/gps", gpsRoute);
 app.use("/api/v1/sequences/graphs", graphsRoute);
