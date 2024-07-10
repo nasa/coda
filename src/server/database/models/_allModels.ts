177101051084b404856d365125676be58109edd9
lmcsherr <luke.a.mcsherry@nasa.gov>
2024-07-10 10:33:10 -0500

    addition of ancillary data to db

--- src/server/database/models/_allModels.ts
+++ src/server/database/models/_allModels.ts
@@ -2,6 +2,8 @@
 // The order of imports is important. Models that are referenced by other models must be imported first.
 import { GPXTracks_db } from "./gpxTracks.model";
 import { MediaOverride_db } from "./mediaOverride.model";
+import { AncillaryDataSource_db } from "./ancillaryData.model";
 
 export { GPXTracks_db };
 export { MediaOverride_db };
+export { AncillaryDataSource_db };
---
+++ src/server/database/models/ancillaryData.model.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { types as MikroTypes } from "@mikro-orm/core";
+
+@Entity()
+export class AncillaryDataSource_db implements AncillaryDataSource_db_type {
+  @PrimaryKey({ type: MikroTypes.integer })
+  id!: number;
+
+  @Property({ type: MikroTypes.text })
+  date!: string;
+  @Property({ type: MikroTypes.text })
+  source!: "ISS" | "TEST_EVENTS" | "NBL" | "ARTEMIS";
+  @Property({ type: MikroTypes.text })
+  type!: "graphs";
+  @Property({ type: MikroTypes.text })
+  url!: string;
+}
