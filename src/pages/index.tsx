3b98fd888a56d3f878962ae8158ef43c9f55c2b5
John Cox <john.l.cox@nasa.gov>
2024-07-30 15:06:24 -0500

    refactor: start of allowing diverse images for background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,24 @@ import styles from "./index.module.css";
 
 export default function Index() {
   const navigate = useNavigate();
+  // ? It technically works,
+  // ? but I dont want to hardcode some images in this file. try might create some sort of list component (/images/background_img_list.tsx)?
+  // ! testing purposes only
+  const images: string[] = [
+    "https://io.jsc.nasa.gov/photos/12558/hires/iss057e106068.jpg",
+    "https://io.jsc.nasa.gov/photos/13524/hires/iss068e059945.jpg",
+    "https://io.jsc.nasa.gov/photos/13476/hires/iss068e008480.jpg",
+    "https://io.jsc.nasa.gov/photos/13257/hires/iss066e146298.jpg",
+    "https://io.jsc.nasa.gov/photos/13260/hires/iss066e152140.jpg",
+    "https://io.jsc.nasa.gov/photos/12558/hires/iss057e106044.jpg",
+    "https://io.jsc.nasa.gov/photos/11891/hires/iss048e010128.jpg",
+  ];
 
+  const randomImage: string = images[Math.floor(Math.random() * images.length)];
+
+  // ? technically works, is it possible to put this within a css file by passing it a variable?
   return (
-    <div className={styles.main}>
+    <div className={styles.main} style={{ backgroundImage: `url(${randomImage})` }}>
       <title>CODA</title>
       <div className={styles.container}>
         <div className={styles.verticalCenter}>
