1b5698b039c99a7a144438a3588e0d6e7f59990c
lmcsherr <luke.a.mcsherry@nasa.gov>
2024-08-02 15:39:22 -0500

    socket changes

--- src/server/express/server.ts
+++ src/server/express/server.ts
@@ -2,7 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 import { createServer, Server as NetServer } from "http";
 import app from "./restApi";
+import { Server as SocketServer } from "socket.io";
 import { getORM } from "utils/mikro";
+import { globalValues } from "./global";
+import { setupSocketIO } from "./sockets";
 
 const port = 3001;
 
@@ -11,9 +14,21 @@ getORM();
 
 // parent http server
 const server: NetServer = createServer();
-
 // express request handler
 server.on("request", app);
+console.log("*Starting Socket.IO");
+globalValues.socketio = new SocketServer<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>(server, {
+  transports: ["websocket"],
+  path: "/api/v1/socketio",
+  addTrailingSlash: false,
+});
+
+setupSocketIO();
 
 server.listen(port, () => {
   console.log(`http server (re)started on ${port}`);
--- src/server/socket-server/global.ts
+++
@@ -1,9 +0,0 @@
-export const globalValues: GlobalValues = {
-  socketio: null,
-  serverSocketStatus: {
-    visitorsData: [],
-  },
-  ormCache: null,
-  socketInterval: null,
-  emssVideoEnabled: true,
-};
