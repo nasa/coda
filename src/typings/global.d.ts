732408e88923857be6d3399ca6ef5c133e6b6fd9
lmcsherr <luke.a.mcsherry@nasa.gov>
2024-08-07 15:33:23 -0500

    removed extra socketio prop in globalvalues

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -1,7 +1,6 @@
 type GlobalValues = {
   socketio: Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>;
   ormCache: MikroORM<D>;
-  socketio: Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>;
   serverSocketStatus: ServerSocketStatus;
   socketInterval: NodeJS.Timeout;
   emssVideoEnabled: boolean;
