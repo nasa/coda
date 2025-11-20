f914296f659a386b238b7ed56b280a94ea231301
Ben Feist <benjamin.f.feist@nasa.gov>
2025-11-20 09:40:52 -0600

    All package updates

--- src/store/contextProviders/_CombinedProviders.tsx
+++ src/store/contextProviders/_CombinedProviders.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { PlayheadContextProvider } from "store/contextProviders/playheadContext";
 import { HoverPlayheadContextProvider } from "store/contextProviders/hoverPlayheadContext";
+import { CookiesProvider } from "react-cookie";
 
 import { composeProviders } from "utils/context";
 
-const providers = [HoverPlayheadContextProvider, PlayheadContextProvider];
+const CookiesProviderWrapper = ({ children }: { children: React.ReactNode }) => (
+  <CookiesProvider>{children}</CookiesProvider>
+);
+
+const providers = [CookiesProviderWrapper, HoverPlayheadContextProvider, PlayheadContextProvider];
 
 // Define the props type to include 'children'
 const CombinedProviders = ({ children }: { children: React.ReactNode }) => {
